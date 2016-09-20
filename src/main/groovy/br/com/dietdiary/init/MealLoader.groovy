9534a1c1c0cf5b48d8f70ed8b4f0d11bfc15b828
Felipe Rocha <feliperocha.ti@gmail.com>
2016-09-20 12:36:19 -0300

    fix MealLoader dates

--- src/main/groovy/br/com/dietdiary/init/MealLoader.groovy
+++ src/main/groovy/br/com/dietdiary/init/MealLoader.groovy
@@ -25,33 +25,32 @@ class MealLoader implements ApplicationListener<ContextRefreshedEvent>  {
     @Override
     void onApplicationEvent(ContextRefreshedEvent event){
         def date = new Date()
-        date.minus 2
-        def meal = new Meal(date: date, meal: MealType.BREAKFAST,
+
+        def meal = new Meal(date: date - 2, meal: MealType.BREAKFAST,
                 foods: [new Food(name: "Margarine-like, margarine-butter blend, soybean oil and butter", calories: 103, ndbno: "04585"),
                         new Food(name: "Bread stuffing, bread, dry mix", calories: 386, ndbno: "18081"),
                         new Food(name: "MILK MAGIC, CHOCOLATE MILK STRAWS, CHOCOLATE MILK, UPC: 803810234951", calories: 385, ndbno: "45100704")])
         mealRepository.save meal
         log.info "Saved BREAKFAST - id: " + meal.id;
-        def meal2 = new Meal(date: date, meal: MealType.LUNCH,
+        def meal2 = new Meal(date: date - 2, meal: MealType.LUNCH,
                 foods: [new Food(name: "O ORGANICS, QUICK-COOK RICE BLEND, ORGANIC BROWN BASMATI RICE, ORGANIC WHOLE GRAIN RED RICE, UPC: 079893502274", calories: 350, ndbno: "45091352"),
                         new Food(name: "Rolls, hamburger or hotdog, mixed-grain", calories: 263, ndbno: "18351"),
                         new Food(name: "Potatoes, french fried, steak fries, salt added in processing, frozen, as purchased", calories: 133, ndbno: "11411"),
                         new Food(name: "MEIJER, ORANGE JUICE, ORANGE, UPC: 041250646310", calories: 155, ndbno: "45040119")])
         mealRepository.save meal2
         log.info "Saved LUNCH - id: " + meal2.id
-        def meal3 = new Meal(date: date, meal: MealType.DINNER,
+        def meal3 = new Meal(date: date - 2, meal: MealType.DINNER,
                 foods: [new Food(name: "O ORGANICS, QUICK-COOK RICE BLEND, ORGANIC BROWN BASMATI RICE, ORGANIC WHOLE GRAIN RED RICE, UPC: 079893502274", calories: 350, ndbno: "45091352"),
                         new Food(name: "STARKIST, SALMON CREATIONS, LIGHTLY MARINATED PREMIUM SKINLESS BONELESS SALMON, LEMON DILL, UPC: 080000516145", calories: 122, ndbno: "45110268"),
                         new Food(name: "Alcoholic beverage, wine, table, white, Semillon", calories: 82, ndbno: "14126")])
         mealRepository.save meal3
         log.info "Saved DINNER - id: " + meal3.id
-        date.plus 1
-        def meal4 = new Meal(date: date, meal: MealType.LUNCH,
+        def meal4 = new Meal(date: date - 1, meal: MealType.LUNCH,
                 foods: [new Food(name: "McDONALD'S, BIG MAC", calories: 257, ndbno: "21237"),
                         new Food(name: "Beverages, The COCA-COLA company, Hi-C Flashin' Fruit Punch", calories: 45, ndbno: "14065")])
         mealRepository.save meal4
         log.info "Saved LUNCH - id: " + meal4.id
-        def meal5 = new Meal(date: date, meal: MealType.SNACK,
+        def meal5 = new Meal(date: date - 1, meal: MealType.SNACK,
                 foods: [new Food(name: "TACO BELL, Soft Taco with steak", calories: 225, ndbno: "21263")])
         mealRepository.save meal5
         log.info "Saved SNACK - id: " + meal5.id
